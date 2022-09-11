c601915351414fce28ce05357fb68ab246775f9b
abhaykathole10 <abhaykathole10@gmail.com>
2022-09-11 22:55:42 +0530

    Creating a String in JJava

---
+++ String/Concepts/Creation.java
@@ -0,0 +1,15 @@
+public class Creation{
+    public static void main(String[] args) {
+        //There are two methods to create a String
+
+        //Method 1: Using "new" keyword
+
+        String s1 = new String("Abhay");
+        System.out.println(s1);
+
+        //Method 2: Using String literal
+
+        String s2 = "Abhay";
+        System.out.println(s2);
+    }
+}
