552cd8cf435f59c72d141eb878c5e6a50401eaba
abhaykathole10 <abhaykathole10@gmail.com>
2022-09-11 23:04:02 +0530

    concat() method

---
+++ String/Concepts/Methods.java
@@ -0,0 +1,11 @@
+public class Methods {
+    public static void main(String[] args) {
+
+        String s1 = new String("Abhay");
+
+        // 1. concat method
+        s1 = s1.concat("Kathole");
+
+        System.out.println(s1);
+    }
+}
